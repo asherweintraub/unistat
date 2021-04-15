9ff2df1a5e5cfbf65eb98deebc4e9c0f64267a17
asherweintraub <11475126+asherweintraub@users.noreply.github.com>
2021-04-15 01:34:02 -0700

    more responsive card width

--- src/client/components/Card.js
+++ src/client/components/Card.js
@@ -3,7 +3,7 @@ import React from 'react';
 class Card extends React.Component {
   constructor(){
     super();
-    this.classes = "flex flex-col items-center border-2 border-gray shadow-md rounded-lg bg-white p-6 w-11/12 md:w-2/5 my-2";
+    this.classes = "flex flex-col items-center border-2 border-gray shadow-md rounded-lg bg-white p-6 w-11/12 md:w-5/6 lg:w-2/5 my-2";
   }
 }
 
