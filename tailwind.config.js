e2eba09284e05eaf90d97763494839b5a703be2d
asherweintraub <11475126+asherweintraub@users.noreply.github.com>
2021-04-14 22:56:10 -0700

    added tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+const colors = require('tailwindcss/colors');
+
+module.exports = {
+  mode: 'jit',
+  darkMode: 'media',
+  theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      black: colors.black,
+      white: colors.white,
+      gray: colors.warmGray,
+      yellow: colors.amber,
+      green: colors.emerald,
+      blue: colors.blue,
+      red: colors.rose,
+    }
+  }
+}
