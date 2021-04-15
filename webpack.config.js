56ad123f216bb764d51657b9b66590973b63c0d6
asherweintraub <11475126+asherweintraub@users.noreply.github.com>
2021-04-14 18:29:54 -0700

    added babel

--- webpack.config.js
+++ webpack.config.js
@@ -6,5 +6,21 @@ module.exports = (env,) => {
     output: {
       filename: 'bundle.js',
     },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: [
+            {
+              loader: 'babel-loader',
+              options: {
+                cacheDirectory: true,
+              },
+            },
+          ],
+        },
+      ],
+    },
   }
 }
