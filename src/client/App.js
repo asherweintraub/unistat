64e59425fa75a8598bf50aaf3812dfdf90797b58
asherweintraub <11475126+asherweintraub@users.noreply.github.com>
2021-04-15 01:30:33 -0700

    login component and styling

--- src/client/App.js
+++ src/client/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from './components/Header';
+import Login from './components/Login';
 
 const App = () => {
   return (
     <div className="flex flex-col items-center justify-center bg-blue-900 min-h-screen p-8">
       <Header />
+      <Login />
     </div>
   );
 }
