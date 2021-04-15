bb3ef276b370428c2c2953ab3ef1e04882e93c61
asherweintraub <11475126+asherweintraub@users.noreply.github.com>
2021-04-14 23:17:17 -0700

    use React StrictMode

--- src/client/index.js
+++ src/client/index.js
@@ -3,4 +3,4 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import "tailwindcss/tailwind.css";
 
-ReactDOM.render(<App />, document.querySelector('#app'));
+ReactDOM.render(<React.StrictMode><App /></React.StrictMode>, document.querySelector('#app'));
