4afd7d203b06c3ff752b093d3f959907160e5b5e
asherweintraub <11475126+asherweintraub@users.noreply.github.com>
2021-04-17 13:16:44 -0700

    get college lists with niche logins

--- src/api/index.js
+++ src/api/index.js
@@ -4,8 +4,41 @@ const puppeteer = require('puppeteer-extra');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  console.log('get at /index');
-});
+const StealthPlugin = require('puppeteer-extra-plugin-stealth');
+puppeteer.use(StealthPlugin());
+
+const nicheLogin = async (browser, user, pass) => {
+  const page = await browser.newPage();
+  await page.goto('https://www.niche.com/account/login/');
+  await page.type('#loginEmail', user);
+  await page.type('#loginPassword', pass);
+  await page.click('button[type="submit"]');
+  await page.waitForNavigation();
+  await page.close();
+}
+
+app.get('/list', async (req, res) => {
+  const browser = await puppeteer.launch({ headless: true, slowMo: 10 });
+  await nicheLogin(browser, req.query.username, req.query.password);
+  const page = await browser.newPage();
+  await page.goto('https://www.niche.com/account/list/');
+  const table = await page.$('table.account-entity-table tbody');
+  const list = await table.evaluate(element => {
+
+    let children = Array.from(element.children);
+
+    return children.map(child => {
+      return {
+        title: child.querySelector('.entity-row__title').innerText,
+        status: child.querySelector('.status-select__pill').innerText,
+        url: child.querySelectorAll('.popover-item')[0].href,
+        img: child.querySelector('.entityImage__figure__image').style.backgroundImage ? child.querySelector('.entityImage__figure__image').style.backgroundImage.match(/url\("(.*?)"\)/)[1] : null
+      }
+    });
+  });
+
+  res.send(list);
+  await browser.close();
+})
 
 app.listen(port, () => {console.log(`Listening at port ${port}`)});
