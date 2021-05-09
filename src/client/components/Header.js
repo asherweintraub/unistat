05c8125a9e505e3b597e59ae2d441ec6cadffcb8
asherweintraub <11475126+asherweintraub@users.noreply.github.com>
2021-05-09 16:53:20 -0400

    simplified header children rendering syntax

--- src/client/components/Header.js
+++ src/client/components/Header.js
@@ -10,8 +10,12 @@ const Header = (props) => {
       <AcademicCapIcon className="h-16"/>
       <h1 className="text-4xl font-bold mb-3">Unistat</h1>
       <p className="text-left w-full">lorem ipsum dolor sit amet. lorem ipsum dolor sit amet. lorem ipsum dolor sit amet.</p>
-      { props.children ? <hr className="w-full my-3"/> : null }
-      {props.children}
+      { props.children
+        ? <>
+          <hr className="w-full my-3"/>
+          { props.children }
+        </>
+        : null }
     </Card>
   );
 }
