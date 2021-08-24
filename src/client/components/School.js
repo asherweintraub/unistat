c33e89a870a68e5ebdec9b182e1a5c87f02db507
asherweintraub <11475126+asherweintraub@users.noreply.github.com>
2021-08-24 14:41:19 -0400

    show school data

--- src/client/components/School.js
+++ src/client/components/School.js
@@ -4,11 +4,13 @@ import { ArrowRightIcon } from '@heroicons/react/outline';
 import Card from './Card';
 
 const School = ({ school, options }) => {
-  // title, status, url, img
+
   return (
     <Card>
-      { options.images ? <img src={school.img} alt={school.title + " cover image"} className="mb-2"/> : null }
-      <a href={school.url} className="group text-2xl font-bold w-full mb-3">{school.title}<ArrowRightIcon className="inline h-5 ml-1 transform -translate-x-0.5 group-hover:translate-x-1 opacity-0 group-hover:opacity-100 transition-all"/></a>
+      <div className="w-full grid grid-cols-fit-28 gap-3 grid-flow-row-dense">
+        {Array.from(options).map((option) => <>{option[1][0] ? option[1][1](school) : null}</>)}
+      </div>
+      <a href={school.url} className="text-sm w-full italic mt-3">see on Niche...</a>
     </Card>
   );
 }
